76be11409450e78338458849b0918c9284404085
tamiltechguru <tamiltechguru2@gmail.com>
2025-05-12 22:09:24 +0530

    optimize multiples of 3 or 5

--- Multiples of 3 or 5/multiples_of_3_or_5.py
+++ Multiples of 3 or 5/multiples_of_3_or_5.py
@@ -1,4 +1,2 @@
 def solution(number):
-    if number < 0:
-        return 0
-    return sum(x for x in range(number) if x % 3 == 0 or x % 5 == 0)
+    return 0 if number < 0 else sum(x for x in range(number) if x % 3 == 0 or x % 5 == 0)
